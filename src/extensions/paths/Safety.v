f6fc4bec8b9e66a0785cc3182c099bbf9fde7d40
Marianna Rapoport <mrapoport@uwaterloo.ca>
2019-01-17 17:45:03 -0500

    working on getting a general wf condition in val_typing by syntactic reasoning about type in def typing

--- src/extensions/paths/Safety.v
+++ src/extensions/paths/Safety.v
@@ -54,6 +54,129 @@ Proof.
   - pose proof (pf_invert_fld _ _ Hp) as [V Hp']. eauto.
 Qed.
 
+Lemma def_typing_rhs z bs P G a t q :
+      z; bs; P; G ⊢ {a := t} : {a ⦂ typ_sngl q} ->
+      exists T, G ⊢ trm_path q : T.
+Proof.
+  intros Hd. dependent induction Hd; eauto. inversion H0.
+Qed.
+
+Reserved Notation "x '==>' T '=' bs '=>' U" (at level 40, T at level 20).
+
+Inductive lookup_fields_typ : var -> typ -> list trm_label -> typ -> Prop :=
+| lft_empty : forall x T,
+    x ==> T =nil=> T
+| lft_cons : forall T bs U x a S,
+    x ==> T =bs=> typ_bnd U ->
+    record_has (open_typ_p (p_sel (avar_f x) bs) U) {a ⦂ S} ->
+    x ==> T =a::bs=> S
+
+where "x '==>' T '=' bs '=>' U" := (lookup_fields_typ x T bs U).
+
+Hint Constructors lookup_fields_typ.
+
+
+Lemma pf_to_lft G x bs T S U :
+  inert (G & x ~ T) ->
+  G & x ~ T ⊢! p_sel (avar_f x) bs : S ⪼ U ->
+  x ==> T =bs=> S.
+Proof.
+  intros Hi Hp. dependent induction Hp; eauto.
+  - apply binds_push_eq_inv in H0 as ->. auto.
+  - simpl_dot. specialize (IHHp _ _ _ _ Hi JMeq_refl eq_refl).
+    pose proof (pf_bnd_T2 Hi Hp) as [V ->].
+    econstructor; eauto. apply* pf_record_has_U.
+Qed.
+
+Lemma lft_inert x T bs U :
+  inert_typ T ->
+  x ==> T =bs=> typ_bnd U ->
+  record_type U.
+Proof. Admitted.
+
+Lemma lft_unique x S bs T U :
+  inert_typ S ->
+  x ==> S =bs=> T ->
+  x ==> S =bs=> U ->
+  T = U.
+Proof.
+  intros Hi Hl1. gen U. induction Hl1; introv Hl2.
+  - inversion Hl2; auto.
+  - inversions Hl2. specialize (IHHl1 Hi _ H4) as [= ->].
+    pose proof (lft_inert Hi Hl1).
+    eapply unique_rcd_trm. apply* open_record_type_p. eauto. eauto.
+Qed.
+
+Lemma lft_typ_dec_inv x S bs A T U cs V :
+  x ==> S =bs=> typ_bnd (typ_rcd {A >: T <: U}) ->
+  x ==> S =cs++bs=> V ->
+  cs = nil.
+Proof.
+  gen x S bs A T U V. induction cs; introv Hl1 Hl2; auto.
+  rewrite <- List.app_comm_cons in Hl2.
+  inversions Hl2. specialize (IHcs _ _ _ _ _ _ _ Hl1 H3) as ->.
+  rewrite List.app_nil_l in H3.
+  assert (U0 = typ_rcd {A >: T <: U}) as -> by admit.
+  inversions H5.
+Qed.
+
+Lemma defs_typing_rhs z bs P G ds T a q U S cs :
+  z; bs; P; G ⊢ ds :: T ->
+  inert_typ S ->
+  z ==> S =bs=> typ_bnd T ->
+  z ==> S =cs++bs=> typ_bnd U ->
+  record_has (open_typ_p (p_sel (avar_f z) (cs++bs)%list) U) {a ⦂ typ_sngl q} ->
+  exists T, G ⊢ trm_path q : T.
+Proof.
+  induction 1; intros Hin Hl1 Hl2 Hr; eauto.
+  - destruct D; inversions H.
+    + pose proof (lft_typ_dec_inv _ Hl1 Hl2) as ->.
+      rewrite List.app_nil_l in Hl2.
+      pose proof (lft_unique Hin Hl1 Hl2) as [= <-].
+      inversion Hr.
+    + admit. (*todo
+
+        change lambda def rule to make U forall type*)
+    +
+
+
+
+  - destruct D; inversions Hr. inversions H. inversion H8. apply* def_typing_rhs.
+  - inversions Hr; eauto. inversions H5. inversions H0.
+    + inversion H10.
+    + eauto.
+Qed.
+
+Lemma defs_typing_rhs z bs P G ds T a q :
+  z; bs; P; G ⊢ ds :: T ->
+  record_has T {a ⦂ typ_sngl q} ->
+  exists T, G ⊢ trm_path q : T.
+Proof.
+  induction 1; intros Hr.
+  - destruct D; inversions Hr. inversions H. inversion H8. apply* def_typing_rhs.
+  - inversions Hr; eauto. inversions H5. inversions H0.
+    + inversion H10.
+    + eauto.
+Qed.
+
+Inductive wf_env_gen : ctx -> Prop :=
+  | wfe_empty : wf_env_gen empty
+  | wfe_push : forall (G : ctx) (x : var) (T : typ),
+               wf_env_gen G ->
+               x # G ->
+               (forall (bs : fields) (q : path),
+                G & x ~ T ⊢! p_sel (avar_f x) bs : typ_sngl q ⪼ typ_sngl q -> exists U, G & x ~ T ⊢ trm_path q : U) ->
+               wf_env_gen (G & x ~ T).
+
+Hint Constructors wf_env_gen.
+
+Lemma wf_env_to_wf_gen G :
+  wf_env G -> wf_env_gen G.
+Proof.
+  induction 1; auto.
+  constructor*. introv Hp. specialize (H1 _ _ Hp) as [U Hq]. eexists. apply* precise_to_general2.
+Qed.
+
 Lemma val_typing G x v T :
   inert G ->
   wf_env G ->
@@ -71,6 +194,10 @@ Proof.
     + apply pf_sngl in Hp as [? [? [=]%pf_binds%binds_push_eq_inv]]; auto.
   - exists (typ_bnd T). repeat split*. pick_fresh z. assert (z \notin L) as Hz by auto.
     specialize (H z Hz). assert (z # G) as Hz' by auto.
+    assert (wf_env_gen (G & z ~ open_typ z T)) as Hwfgen. {
+      constructor*. apply* wf_env_to_wf_gen.
+      introv Hp.
+
     admit.
   - specialize (IHHv _ Hi Hwf eq_refl Hx). destruct_all. eauto.
 Qed.
