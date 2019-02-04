252c000f3e8ca27f2f478bc270765551c768b2cd
Marianna Rapoport <mrapoport@uwaterloo.ca>
2019-02-04 16:37:09 -0500

    simplifying examples, adding singleton-motivating example

--- src/extensions/paths/ListExample.v
+++ src/extensions/paths/ListExample.v
@@ -1,17 +1,4 @@
-Require Import Definitions Binding Weakening.
-Require Import List.
-
-Notation this := (p_sel (avar_b 0) nil).
-Notation super := (p_sel (avar_b 1) nil).
-Notation ssuper := (p_sel (avar_b 2) nil).
-Notation sssuper := (p_sel (avar_b 3) nil).
-Notation ssssuper := (p_sel (avar_b 4) nil).
-Notation sssssuper := (p_sel (avar_b 5) nil).
-
-Notation lazy t := (defv (λ(⊤) t)).
-Notation Lazy T := (∀(⊤) T).
-
-Notation "d1 'Λ' d2" := (defs_cons d1 d2) (at level 40).
+Require Import ExampleTactics.
 
 Section ListExample.
 
@@ -40,40 +27,34 @@ Definition t :=
             defs_nil Λ
             { List ⦂= μ (ListType ssuper) } Λ
             { Nil := defv (λ(typ_rcd { A >: ⊥ <: ⊤ })
-                            trm_let (* result = *)
-                            (trm_val (ν(ListTypeA sssuper (super↓A) (super↓A) super)
-                                       defs_nil Λ
-                                       { A ⦂= super↓A } Λ
-                                       { head := defv (λ(⊤) trm_app super•head this) } Λ
-                                       { tail := defv (λ(⊤) trm_app super•tail this) }))
-                            (trm_path this)) } Λ
+                            let_trm (trm_val (ν(ListTypeA sssuper (super↓A) (super↓A) super)
+                                               defs_nil Λ
+                                               { A ⦂= super↓A } Λ
+                                               { head := defv (λ(⊤) trm_app super•head this) } Λ
+                                               { tail := defv (λ(⊤) trm_app super•tail this) }))) } Λ
             { Cons := defv (λ(typ_rcd { A >: ⊥ <: ⊤ })
                    trm_val (λ(this↓A)
                    trm_val (λ(ssuper↓List ∧ typ_rcd { A >: ⊥ <: super↓A })
-                             trm_let (* result = *)
-                             (trm_val (ν(ListTypeA sssssuper (sssuper↓A) (sssuper↓A) ssssuper)
+                             let_trm (trm_val (ν(ListTypeA sssssuper (sssuper↓A) (sssuper↓A) ssssuper)
                                         defs_nil Λ
                                         { A ⦂= sssuper↓A } Λ
                                         { head := lazy (trm_path sssuper) } Λ
-                                        { tail := lazy (trm_path ssuper)}))
-                             (trm_path this)))) }).
-
+                                        { tail := lazy (trm_path ssuper)}))))) }).
 
 Lemma list_typing :
   empty ⊢ t : μ ListObjType.
 Proof.
-  fresh_constructor. repeat apply ty_defs_cons; auto.
+  fresh_constructor. repeat apply ty_defs_cons; crush.
   - Case "Nil".
-    constructor. fresh_constructor. simpl. repeat case_if.
-    unfold open_trm, open_typ. simpl. repeat case_if.
+    constructor. fresh_constructor. crush.
     (* ⊢ let result = ν(ListType){A}{head}{tail} in result : List ∧ {A <:} *)
     fresh_constructor.
     + (* ⊢ ν(ListType){A}{head}{tail} in result : μ(ListType) *)
-      fresh_constructor. repeat apply ty_defs_cons; simpl; auto.
+      fresh_constructor. repeat apply ty_defs_cons; crush.
       * SCase "head".
-        constructor. fresh_constructor. case_if. unfold open_trm, open_typ. simpl. repeat case_if. simpl. case_if.
+        constructor. fresh_constructor. crush.
         assert (p_sel (avar_f y0) nil ↓ A = open_typ_p (p_sel (avar_f y1) nil) (p_sel (avar_f y0) nil ↓ A))
-          as Heq by (unfold open_typ_p; auto).
+          as Heq by crush.
         rewrite Heq. eapply ty_all_elim.
         ** rewrite proj_rewrite. apply ty_new_elim.
            eapply ty_sub; try solve [constructor*].
@@ -83,10 +64,8 @@ Proof.
         ** eapply ty_sub.
            { constructor*. }
            auto.
-      * unfold defs_hasnt. simpl. case_if*.
       * SCase "tail".
-        repeat case_if. constructor. fresh_constructor.
-        unfold open_trm, open_typ. simpl. repeat case_if.
+        constructor. fresh_constructor. crush.
         assert ((p_sel (avar_f z) nil ↓ List) ∧ typ_rcd {A >: ⊥ <: p_sel (avar_f y0) nil ↓ A} =
                 open_typ_p (p_sel (avar_f y1) nil)
                            ((p_sel (avar_f z) nil ↓ List) ∧ typ_rcd {A >: ⊥ <: p_sel (avar_f y0) nil ↓ A}))
@@ -97,100 +76,87 @@ Proof.
         ** eapply ty_sub.
            { constructor*. }
            auto.
-      * unfold defs_hasnt. simpl. repeat case_if; auto.
     + (* y0: ListType ⊢ y0: List ∧ {A<:} *)
-      match goal with
-      | H: _ |- ?G' ⊢ _ : _ =>
-        remember G' as G
-      end.
-      unfold open_trm. simpl. case_if.
-      remember (p_sel (avar_f y0) nil) as py0.
-      assert (G ⊢ trm_path py0 : typ_rcd {A >: p_sel (avar_f y) nil ↓ A <: p_sel (avar_f y) nil ↓ A}) as Hpy1. {
-        rewrite Heqpy0, HeqG.
-        eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. eapply subtyp_trans; apply subtyp_and11.
-      }
-      assert (G ⊢ trm_path py0 : typ_rcd { head ⦂ Lazy (p_sel (avar_f y0) nil ↓ A) }) as Hpy2. {
-        rewrite Heqpy0, HeqG.
-        eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. eapply subtyp_trans. apply subtyp_and11. eauto.
-      }
-      assert (G ⊢ trm_path py0 : typ_rcd {tail ⦂ Lazy ((p_sel (avar_f z) nil ↓ List) ∧
-                                                       typ_rcd {A >: ⊥ <: p_sel (avar_f y0) nil ↓ A})}) as Hpy3. {
-        rewrite Heqpy0, HeqG.
-        eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. apply subtyp_and12.
-      }
-      apply ty_and_intro.
-      * apply ty_sub with (T:=μ ListType (p_sel (avar_f z) nil)).
-        ** apply ty_rec_intro. unfold open_typ_p. simpl. case_if.
-           apply ty_and_intro.
-           { apply ty_and_intro.
-             - apply (ty_sub Hpy1). eauto.
-             - rewrite Heqpy0 in *. auto.
-           }
-           rewrite Heqpy0 in *. auto.
-        ** eapply subtyp_sel2.
-           eapply ty_sub.
-           {
-             rewrite HeqG.
-             constructor*.
-           }
-           eapply subtyp_trans; apply subtyp_and11.
-      * eapply ty_sub. apply Hpy1. apply subtyp_typ; auto.
-  - unfold defs_hasnt. simpl. case_if. auto.
+     remember_ctx G.
+     unfold open_trm. simpl. case_if.
+     remember (p_sel (avar_f y0) nil) as py0.
+     assert (G ⊢ trm_path py0 : typ_rcd {A >: p_sel (avar_f y) nil ↓ A <: p_sel (avar_f y) nil ↓ A}) as Hpy1. {
+       rewrite Heqpy0, HeqG.
+       eapply ty_sub. apply ty_rec_elim. constructor*. crush.
+       eapply subtyp_trans; apply subtyp_and11.
+     }
+     assert (G ⊢ trm_path py0 : typ_rcd { head ⦂ Lazy (p_sel (avar_f y0) nil ↓ A) }) as Hpy2. {
+       rewrite Heqpy0, HeqG.
+       eapply ty_sub. apply ty_rec_elim. constructor*.
+       crush. eapply subtyp_trans. apply subtyp_and11. eauto.
+     }
+     assert (G ⊢ trm_path py0 : typ_rcd {tail ⦂ Lazy ((p_sel (avar_f z) nil ↓ List) ∧
+                                                      typ_rcd {A >: ⊥ <: p_sel (avar_f y0) nil ↓ A})}) as Hpy3. {
+       rewrite Heqpy0, HeqG.
+       eapply ty_sub. apply ty_rec_elim. constructor*.
+       crush.
+     }
+     apply ty_and_intro.
+     * apply ty_sub with (T:=μ ListType (p_sel (avar_f z) nil)).
+       ** apply ty_rec_intro. crush.
+          apply ty_and_intro.
+          { apply ty_and_intro.
+            - apply (ty_sub Hpy1). eauto.
+            - rewrite Heqpy0 in *. auto.
+          }
+          rewrite Heqpy0 in *. auto.
+       ** eapply subtyp_sel2.
+          eapply ty_sub.
+          {
+            rewrite HeqG.
+            constructor*.
+          }
+          eapply subtyp_trans; apply subtyp_and11.
+     * eapply ty_sub. apply Hpy1. apply subtyp_typ; auto.
   - Case "Cons".
-    constructor. do 4 (fresh_constructor; unfold open_trm, open_typ; simpl; repeat case_if).
+    constructor. do 4 (fresh_constructor; crush).
     (* ⊢ let result = ν(ListType){A}{head}{tail} in result : List ∧ {A <:} *)
     + (* ⊢ ν(ListType){A}{head}{tail} in result : μ(ListType) *)
-      fresh_constructor. repeat apply ty_defs_cons; simpl; auto.
+      fresh_constructor. repeat apply ty_defs_cons; crush.
       * SCase "head".
-        case_if.
-        constructor. fresh_constructor. unfold open_trm, open_typ. simpl.
-        case_if.
+        constructor. fresh_constructor. crush.
         eapply ty_sub.
         { constructor*. }
         eapply subtyp_sel2. eapply ty_sub.
         constructor*. eapply subtyp_trans. apply subtyp_and11. eauto.
-      * unfold defs_hasnt. simpl. case_if. auto.
       * SCase "tail".
-        constructor. fresh_constructor. unfold open_trm, open_typ. simpl. case_if.
+        constructor. fresh_constructor. crush.
         apply ty_and_intro.
         ** eapply ty_sub. constructor*. eauto.
         ** eapply ty_sub. eapply ty_sub. constructor*. apply subtyp_and12. apply subtyp_typ.
            auto. auto.
-      * unfold defs_hasnt. simpl. repeat case_if. auto.
     + (* y2: ListType ⊢ y2: List ∧ {A<:} *)
-       match goal with
-      | H: _ |- ?G' ⊢ _ : _ =>
-        remember G' as G
-      end.
-      unfold open_trm. simpl. case_if.
+      remember_ctx G. crush.
       remember (p_sel (avar_f y2) nil) as py0.
       assert (G ⊢ trm_path py0 : typ_rcd {A >: p_sel (avar_f y) nil ↓ A <: p_sel (avar_f y) nil ↓ A}) as Hpy1. {
         rewrite Heqpy0, HeqG.
         eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. eapply subtyp_trans; apply subtyp_and11.
+        crush. eapply subtyp_trans; apply subtyp_and11.
       }
       assert (G ⊢ trm_path py0 : typ_rcd { head ⦂ Lazy (p_sel (avar_f y2) nil ↓ A) }) as Hpy2. {
         rewrite Heqpy0, HeqG.
         eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. eapply subtyp_trans. apply subtyp_and11. eauto.
+        crush. eapply subtyp_trans. apply subtyp_and11. eauto.
       }
       assert (G ⊢ trm_path py0 : typ_rcd {tail ⦂ Lazy ((p_sel (avar_f z) nil ↓ List) ∧
                                                        typ_rcd {A >: ⊥ <: p_sel (avar_f y) nil ↓ A})}) as Hpy3. {
         rewrite Heqpy0, HeqG.
         eapply ty_sub. apply ty_rec_elim. constructor*.
-        unfold open_typ_p. simpl. case_if. apply subtyp_and12.
+        crush.
       }
       apply ty_and_intro.
       * apply ty_sub with (T:=μ ListType (p_sel (avar_f z) nil)).
-        ** rewrite Heqpy0 in *. apply ty_rec_intro. unfold open_typ_p. simpl. case_if.
+        ** rewrite Heqpy0 in *. apply ty_rec_intro. crush.
            apply ty_and_intro.
            { apply ty_and_intro; auto. apply (ty_sub Hpy1). eauto. }
            eapply ty_sub. apply Hpy3.
            constructor. fresh_constructor.
-           unfold open_typ. simpl. apply subtyp_and2.
+           crush. apply subtyp_and2.
            *** apply subtyp_and11.
            *** eapply subtyp_trans. apply subtyp_and12. apply subtyp_typ. auto.
                eapply subtyp_sel2. apply* weaken_ty_trm. rewrite HeqG. repeat apply* ok_push.
@@ -198,7 +164,6 @@ Proof.
            eapply subtyp_sel2. eapply ty_sub. constructor*.
            eapply subtyp_trans. apply subtyp_and11. eapply subtyp_and11.
       * eapply ty_sub. apply Hpy1. apply subtyp_typ; auto.
-  - simpl. repeat case_if. unfold defs_hasnt. simpl. repeat case_if. auto.
 Qed.
 
 End ListExample.
